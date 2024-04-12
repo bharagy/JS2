6e09ff7223b1cff56a3b64b14891979a1864e49f
bharagy <145537623+bharagy@users.noreply.github.com>
2024-04-12 15:14:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let name = "f";
+
+switch (name){
+    case "a":
+    console.log("vowels");
+    break;
+    case "e":
+        console.log("vowels");
+    break;
+    case "i":
+        console.log("vowels");
+    break;
+    case"o":
+        console.log("vowels");
+    break;
+    case "u":
+        console.log("vowels");
+    break;
+    default:
+        console.log("not a vowel");
+        break;
+    }
